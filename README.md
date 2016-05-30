b8d0f63304dd69bf92112c48e17525fd9af51740
ebarreat <erick.b1337@gmail.com>
2016-05-29 23:50:47 -0500

    Actual Base

---
+++ README.md
@@ -0,0 +1,4 @@
+Prosked
+=======
+
+A Symfony project created on May 30, 2016, 1:47 am.
---
+++ src/AppBundle/Controller/homeController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ebarreat
+ * Date: 5/29/2016
+ * Time: 11:00 PM
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class homeController
+{
+    /**
+     * @Route()
+     */
+    public function showAction(){
+
+    }
+}
